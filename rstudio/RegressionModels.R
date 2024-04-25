f13e93d763b58e328ad8e6019767caa445da39de
tobuya <thomasobuya95@gmail.com>
2024-04-25 11:53:16 +0300

    Conduct simple linear regression on a categorical and numerical explanatory variables

--- rstudio/RegressionModels.R
+++ rstudio/RegressionModels.R
@@ -31,3 +31,48 @@ ggplot(evals_ch5, aes(x = bty_avg, y = score)) +
   geom_smooth(method = "lm", se = FALSE)
 
 #Simple Linear Regression
+#~ Fit the regression model
+score_model <- lm(score ~ bty_avg, data = evals_ch5)
+#~ Get the regression table
+get_regression_table(score_model)
+
+regression_points <- get_regression_points(score_model)
+regression_points
+#~ Compute the sum of square residuals
+regression_points %>%
+  mutate(squared_residuals = residual^2) %>%
+  summarize(sum_of_squared_residuals = sum(squared_residuals))
+
+#Exploring the Gap minder data set
+gapminder2007 <- gapminder %>%
+  filter(year == 2007) %>%
+  select(country, lifeExp, continent, gdpPercap)
+
+glimpse(gapminder2007)
+gapminder2007 %>%
+  sample_n(size = 5)
+gapminder2007 %>%
+  select(lifeExp, continent) %>%
+  skim()
+
+ggplot(gapminder2007, aes(x = lifeExp)) +
+  geom_histogram(binwidth = 5, color = "white") +
+  labs(
+    x = "Life expectancy", y = "Number of countries",
+    title = "Histogram of life expectancy in 2007") +
+  facet_wrap(~continent, nrow = 2)
+
+ggplot(gapminder2007, aes(x = continent, y = lifeExp)) +
+  geom_boxplot() +
+  labs(x = "Continent", y = "Life Expecatancy", title = "Life Expectancy by continent in 2007")
+
+lifeExp_by_continent <- gapminder2007 %>%
+  group_by(continent) %>%
+  summarize(median = median(lifeExp), mean = mean(lifeExp))
+lifeExp_by_continent
+
+#Linear Regression: Numerical and Categorical explanatory variable
+lifeExp_model <- lm(lifeExp ~ continent, data = gapminder2007)
+get_regression_table(lifeExp_model)
+regression_points <- get_regression_points(lifeExp_model, ID = "country")
+regression_points
