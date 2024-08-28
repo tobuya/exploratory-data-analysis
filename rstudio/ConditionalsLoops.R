ba2d9a60d00d29bc647e90ba051fa146ad8b39a7
tobuya <thomasobuya95@gmail.com>
2024-08-28 22:55:49 +0300

    Add break, next and for loop

--- rstudio/ConditionalsLoops.R
+++ rstudio/ConditionalsLoops.R
@@ -24,3 +24,51 @@ while(ctr <= 7) {
   print(paste("Current ctrl value is:", ctr))
   ctr <- ctr + 1
 }
+
+speed <- 64
+while(speed >30){
+  print(paste("The current speed is:", speed))
+  if(speed > 48) {
+    print("Slow down big time!")
+    speed <- speed - 11
+  }else {
+    print("Slow down")
+    speed <- speed - 6
+  }
+}
+
+# For loop
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+for(linked in linkedin) {
+  print(paste("Current linkedin view is:", linked))
+}
+
+for(i in 1:length(linkedin)) {
+  print(paste("Current linkedin view is:", linkedin[i]))
+}
+
+for (i in 1:nrow(ttt)) {
+  for (j in 1:ncol(ttt)) {
+    print(paste("On row", i, "and column", j, "the board contains", ttt[i,j]))
+  }
+}
+
+for(li in linkedin) {
+  if(li > 10) {
+    print("You are popular!")
+  }else {
+    print("Be more visible.")
+  }
+  
+  if(li > 16) {
+    print("You are a rockstar!")
+    break
+  }
+  
+  if(li < 4) {
+    print("You need to be more active!")
+    next
+  }
+  
+  print(li)
+}
